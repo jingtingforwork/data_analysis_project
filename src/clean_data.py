a32f953717848f6e4019b96e6d2dcbd29e8b81c3
jingtingforwork <jingtingforwork@gmail.com>
2025-11-08 17:46:06 +0800

    add load_data & clean_data for automatic data download and handle null values.

---
+++ src/clean_data.py
@@ -0,0 +1,29 @@
+def clean_data(df):
+    import pandas as pd
+
+    ## 填補 float 缺失值
+
+    # 先把 Max_BPM 轉 float
+    df['Max_BPM'] = pd.to_numeric(df['Max_BPM'], errors='coerce')
+    # 找到所有dtype='float64'的column
+    numeric_cols = df.select_dtypes(include=['float64']).columns
+    # 用「平均數」填補缺失值
+    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+
+
+    ## 填補 object 缺失值
+
+    # 找出欄位'Gender'中最常出現的值
+    new_Gender = df['Gender'].mode()[0]
+    # 找出欄位'Workout_Type'中最常出現的值
+    new_Workout_Type = df['Workout_Type'].mode()[0]
+    # 用「眾數」填補缺失值
+    df['Gender'] = df['Gender'].fillna(new_Gender)
+    df['Workout_Type'] = df['Workout_Type'].fillna(new_Workout_Type)
+
+    # 另存整理好的datafram
+    df.to_csv('/Users/jingting/Documents/GitHub/data_analysis_project/data/cleaned_data.csv', index=False)
+
+    return df
+
+
---
+++ src/load_data.py
@@ -0,0 +1,33 @@
+
+def load_data():
+    '''
+    利用kaggle API下載最新dataset，並自動轉成dataframe
+    '''
+    import os
+    import pandas as pd
+    from kaggle.api.kaggle_api_extended import KaggleApi
+
+    # 初始化 Kaggle API
+    api = KaggleApi()
+    api.authenticate()
+
+    # 資料集 ID
+    dataset = "nadeemajeedch/fitness-tracker-dataset"
+
+    # 指定下載路徑
+    raw_data_path = "/Users/jingting/Documents/GitHub/data_analysis_project/data/raw"
+    os.makedirs(raw_data_path, exist_ok=True) # 若資料夾不存在就建立
+
+    # 下載資料集並解壓到 data/raw/
+    api.dataset_download_files(dataset, path=raw_data_path, unzip=True)
+
+    # 找出 CSV 檔案
+    csv_files = [f for f in os.listdir(raw_data_path) if f.endswith(".csv")]
+
+    if csv_files:
+        # 讀取第一個 CSV 檔案
+        csv_path = os.path.join(raw_data_path, csv_files[0])
+        df = pd.read_csv(csv_path)
+        return df
+    else:
+        print("找不到 CSV 檔案")
