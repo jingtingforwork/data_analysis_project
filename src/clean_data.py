fdf1263772a5b7bf33579a5891593ef2dfe758cd
jingtingforwork <jingtingforwork@gmail.com>
2025-11-18 11:13:59 +0800

    Adjust the program to conform to the new data

--- src/clean_data.py
+++ src/clean_data.py
@@ -6,18 +6,17 @@ def clean_data(df):
 
     ## 填補 float 缺失值
 
-    # 先把 “Max_BPM” 轉 float
-    df['Max_BPM'] = pd.to_numeric(df['Max_BPM'], errors='coerce')
-    
-    # 將 dtype='int' 轉 'float64'
-    int_cols = df.select_dtypes(include=['int']).columns
-    df[int_cols] = df[int_cols].astype(float)
-
     # 找到所有dtype='float64'的column
-    numeric_cols = df.select_dtypes(include=['float64']).columns
+    float_cols = df.select_dtypes(include=['float64']).columns
     # 用「平均數」填補缺失值
-    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean()).round(2)
+    df[float_cols] = df[float_cols].fillna(df[float_cols].mean()).round(2)
 
+    ## 填補 int 缺失值
+
+    # 找到所有dtype='int64'的column
+    int_cols = df.select_dtypes(include=['int64']).columns
+    # 用「平均數」填補缺失值
+    df[int_cols] = df[int_cols].fillna(df[int_cols].mean())
 
     ## 填補 object 缺失值
 
@@ -28,12 +27,8 @@ def clean_data(df):
     # 用「眾數」填補缺失值
     df['Gender'] = df['Gender'].fillna(new_Gender)
     df['Workout_Type'] = df['Workout_Type'].fillna(new_Workout_Type)
-
+    # 去除特殊符號
     df['Workout_Type'] = df['Workout_Type'].str.replace('\\n', '').str.replace('\\t', '')
-
-    # 只保留合法值
-    # valid_types = ["Strength", "Cardio", "Yoga", "HIIT"]
-    # df = df[df['Workout_Type'].isin(valid_types)].reset_index(drop=True)
 
 
     return df
